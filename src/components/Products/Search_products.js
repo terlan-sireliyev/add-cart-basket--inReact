9d4cdc965ef792ef721317f6885a624adc5f842b
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-01-10 19:44:09 +0300

    asdas

--- src/components/Products/Search_products.js
+++ src/components/Products/Search_products.js
@@ -3,7 +3,7 @@ import styleSearch from './style/searchInp.module.css'
 export default function Search_products({ onChange, setSort, setSlice, products, setProducts, searchItem, handleInputChange }) {
 
     return (
-        <div>
+        <digit
             <div className={styleSearch.searchInp}>
                 <div className={styleSearch.inputDiv}>
                     <input
